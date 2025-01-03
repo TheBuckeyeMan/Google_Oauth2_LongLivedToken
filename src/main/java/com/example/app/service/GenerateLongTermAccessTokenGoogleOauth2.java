f25874e89e17da9ac3d064d6246eb7c78ad8c564
TheBuckeyeMan <156697236+TheBuckeyeMan@users.noreply.github.com>
2025-01-03 12:03:51 -0500

    Update GenerateLongTermAccessTokenGoogleOauth2.java

--- src/main/java/com/example/app/service/GenerateLongTermAccessTokenGoogleOauth2.java
+++ src/main/java/com/example/app/service/GenerateLongTermAccessTokenGoogleOauth2.java
@@ -34,7 +34,7 @@ public class GenerateLongTermAccessTokenGoogleOauth2 {
                 JSON_FACTORY,
                 clientSecrets,
                 Collections.singletonList("https://www.googleapis.com/auth/youtube.readonly") //Change if you need different access with your token. Ex. TO get video data it will be .readonly or to post it will be .upload
-        ).setAccessType("offline").build();
+        ).setApprovalPrompt("force").setAccessType("offline").build();
 
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8087).setCallbackPath("/oauth2/callback").build(); //Change to the port Specified in Google Cloud
         Credential credential = new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
